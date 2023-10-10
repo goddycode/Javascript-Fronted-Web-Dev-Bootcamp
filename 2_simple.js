ae88a5c59fa26d10ed1845f777949ac2ed2b8998
goddycode <odhisgoddy24@gmail.com>
2023-10-10 13:36:43 +0000

    Adding '2_simple.js'

--- 2_simple.js
+++ 2_simple.js
@@ -1,5 +1,11 @@
-function doMath(x, y) {
+function multiplication(x, y) {
 return x * y;
 }
-const result = doMath(4, 5);
-console.log(result);
+
+function add(x, y) {
+return x + y;
+}
+const product = multiplication(4, 5);
+const addition = add(4,5);
+console.log(multiplication);
+console.log(addition);
